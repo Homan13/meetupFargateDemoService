52d35c5a410a8f718abe8b68fbb4b953e828c232
Homan13 <homan13@gmail.com>
2019-06-20 13:40:52 -0400

    initial commit 2

--- bin/meet_up_fargate_demo_service.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import cdk = require('@aws-cdk/cdk');
-import { MeetUpFargateDemoServiceStack } from '../lib/meet_up_fargate_demo_service-stack';
-
-const app = new cdk.App();
-new MeetUpFargateDemoServiceStack(app, 'MeetUpFargateDemoServiceStack');
